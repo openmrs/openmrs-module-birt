1db98bc4aa10e215179f5216ff2ed13b8efbb2fb
finbrain <finbrain@5bac5841-c719-aa4e-b3fe-cce5062f897a>
2012-05-25 16:04:32 +0000

    Basic rendering of rptdesign to pdf

--- src/org/openmrs/module/birt/report/renderer/BirtTemplateRenderer.java
+++ src/org/openmrs/module/birt/report/renderer/BirtTemplateRenderer.java
@@ -13,16 +13,35 @@
  */
 package org.openmrs.module.birt.report.renderer;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintStream;
+import java.io.Writer;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.eclipse.birt.report.engine.api.EngineException;
+import org.eclipse.birt.report.engine.api.IReportEngine;
+import org.eclipse.birt.report.engine.api.IReportRunnable;
+import org.eclipse.birt.report.engine.api.IRunAndRenderTask;
+import org.openmrs.Cohort;
 import org.openmrs.annotation.Handler;
+import org.openmrs.api.context.Context;
+import org.openmrs.module.birt.BirtReport;
+import org.openmrs.module.birt.BirtReportException;
+import org.openmrs.module.birt.BirtReportService;
+import org.openmrs.module.birt.impl.BirtConfiguration;
 import org.openmrs.module.reporting.common.Localized;
 import org.openmrs.module.reporting.report.ReportData;
+import org.openmrs.module.reporting.report.definition.ReportDefinition;
 import org.openmrs.module.reporting.report.renderer.RenderingException;
+import org.openmrs.module.reporting.report.renderer.ReportRenderer;
 import org.openmrs.module.reporting.report.renderer.ReportTemplateRenderer;
+import org.springframework.util.FileCopyUtils;
 
 /**
  * Report Renderer implementation that supports rendering to a BIRT template
@@ -38,14 +57,101 @@ public class BirtTemplateRenderer extends ReportTemplateRenderer {
 	}
 
 	/**
+	 * @see org.openmrs.report.ReportRenderer#getRenderedContentType(org.openmrs.report.ReportDefinition, java.lang.String)
+	 */
+	public String getRenderedContentType(ReportDefinition schema, String argument) {
+		return "text/html";
+	}
+
+	/**
+	 * @see org.openmrs.report.ReportRenderer#getLinkUrl(org.openmrs.report.ReportDefinition)
+	 */
+	public String getLinkUrl(ReportDefinition schema) {
+		return null;
+	}
+
+	/**
+	 * @see org.openmrs.report.ReportRenderer#getFilename(org.openmrs.report.ReportDefinition)
+	 */
+	public String getFilename(ReportDefinition schema, String argument) {
+		return schema.getName() + ".rptdesign";
+	}
+
+	/**
 	 * @see ReportRenderer#render(ReportData, String, OutputStream)
 	 */
-	public void render(ReportData reportData, String argument, OutputStream out)
-			throws IOException, RenderingException {
+	public void render(ReportData reportData, String argument, OutputStream out) throws IOException, RenderingException {
 
-		log.debug("Attempting to render report with BirtTemplateRenderer");
+		log.warn("Attempting to render report with BirtTemplateRenderer");
 		System.out.println("hello world");
+		   
+		// Get BIRT report service
+		   BirtReportService reportService = (BirtReportService) Context.getService(BirtReportService.class);
+		   
+		   
+		   BirtReport report = new BirtReport();
+		   report.setCohort(new Cohort());
+		   report.setOutputFilename("c:\\save\\report.pdf");
+		   report.setReportDesignPath("c:\\save\\report.rptdesign");
+		   report.setOutputFormat("pdf");
+		   
+		   reportService.generateReport(report);
 
+		   // Get a reference to the report output file to be copied to the response
+		   InputStream fileInputStream = new FileInputStream(report.getOutputFile());
+		   
+		   // Copy report output to response
+		   FileCopyUtils.copy(fileInputStream, out);		   
+		   
 	}
+	
+	/**
+	 * Generate a report based on the attributes of the given report object.
+	 * 
+	 * @param	report	the report to generate
+	 */
+	public void generateReport(BirtReport report) {
+		log.debug("Generating output for report " + report + ", hashcode " + report.hashCode());
+		IRunAndRenderTask task = null;
+		try {     		
+			// Prepares the dataset for use within the BIRT engine 
+			// (i.e. exports csv file, sets username/password for JDBC dataset)
+			//prepareDataset(report);
+
+			// Get the report engine that will be used to render the report
+			IReportEngine engine = BirtConfiguration.getReportEngine();	    		
+
+			// Open the report design
+			IReportRunnable reportRunnable =
+				engine.openReportDesign(report.getReportDesignPath());
+
+			// Create a report rendering task
+			task = engine.createRunAndRenderTask(reportRunnable);
+			task.setParameterValues(report.getParameterValues());
+			task.validateParameters();    		
+			task.setRenderOption(BirtConfiguration.getRenderOption(report));
+			task.run();
+
+			// Add errors to the report object
+			if (task.getErrors() != null && !task.getErrors().isEmpty()) {
+				report.setErrors(task.getErrors());
+			}
+
+			// Set the output file 
+			report.setOutputFile(new File(report.getOutputFilename()));	    	
+
+			log.debug("Output file: " + report.getOutputFile().getAbsolutePath());
+		} 
+		catch (EngineException e) { 
+			log.error("Unable to generate report due to a BIRT Exception: " + e.getMessage(), e);
+			throw new BirtReportException("Unable to generate report due to a BIRT Exception: " + e.getMessage(), e);
+		}
+		finally { 
+			if (task != null) task.close();
+		}
+	}
+
+	
+	
 
 }
