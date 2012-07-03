b954cb15af06c58502e032ea5ffc8502ce127335
finbrain <finbrain@5bac5841-c719-aa4e-b3fe-cce5062f897a>
2012-07-03 13:05:46 +0000

    execute report - design to pdf

--- src/org/openmrs/module/birt/report/renderer/BirtTemplateRenderer.java
+++ src/org/openmrs/module/birt/report/renderer/BirtTemplateRenderer.java
@@ -20,7 +20,10 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -28,6 +31,7 @@ import org.eclipse.birt.report.engine.api.EngineException;
 import org.eclipse.birt.report.engine.api.IReportEngine;
 import org.eclipse.birt.report.engine.api.IReportRunnable;
 import org.eclipse.birt.report.engine.api.IRunAndRenderTask;
+import org.eclipse.birt.report.engine.api.PDFRenderOption;
 import org.eclipse.birt.report.model.api.OdaDataSetHandle;
 import org.eclipse.birt.report.model.api.OdaDataSourceHandle;
 import org.eclipse.birt.report.model.api.ReportDesignHandle;
@@ -35,6 +39,7 @@ import org.eclipse.birt.report.model.api.activity.SemanticException;
 import org.openmrs.annotation.Handler;
 import org.openmrs.api.ReportService;
 import org.openmrs.api.context.Context;
+import org.openmrs.module.birt.BirtDataSetQuery;
 import org.openmrs.module.birt.BirtReport;
 import org.openmrs.module.birt.BirtReportException;
 import org.openmrs.module.birt.BirtReportService;
@@ -55,6 +60,7 @@ import org.openmrs.module.reporting.dataset.DataSetColumn;
 import org.openmrs.module.reporting.dataset.DataSetRow;
 import org.openmrs.Cohort;
 import org.openmrs.module.reporting.indicator.IndicatorResult;
+import org.eclipse.birt.report.model.api.DesignFileException;
 import org.eclipse.birt.report.model.api.ElementFactory;
 import org.eclipse.birt.report.model.api.ReportDesignHandle;
 import org.eclipse.birt.report.model.api.SessionHandle;
@@ -99,54 +105,32 @@ public class BirtTemplateRenderer extends ReportTemplateRenderer {
 	public String getFilename(ReportDefinition schema, String argument) {
 		return schema.getName() + ".pdf";
 	}
+	
 
+	String pathName = null;
+	String pathDir = null;
 	/**
 	 * @see ReportRenderer#render(ReportData, String, OutputStream)
 	 */
 	public void render(ReportData reportData, String argument, OutputStream out) throws IOException, RenderingException {
-		
-		FileOutputStream fos = null;
 		try {
 			log.debug("Attempting to render report with BirtTemplateRenderer");
 			
-			System.out.println("report data " +  reportData.getDataSets());
-
 			ReportDesign design = getDesign(argument);
-			ReportDesignResource r = getTemplate(design);			
-
-			String pathDir = BirtReportUtil.createTempDirectory("reports").getAbsolutePath() + File.separator;
+			ReportDesignResource r = getTemplate(design);
 			
-			BirtReport report = new BirtReport();
-			report.setOutputFormat("pdf");
-			report.setOutputFilename(pathDir + r.getName() + "." + report.getOutputFormat());	
-
-			String pathName = pathDir + r.getResourceFilename();
-			fos = new FileOutputStream(pathName);
-			fos.write(r.getContents());
-
-			//need to get the right prefix for the path to append to fileName
-			report.setReportDesignPath(pathName);
+			pathDir = BirtReportUtil.createTempDirectory("reports").getAbsolutePath() + File.separator;
 			
-			BirtReportServiceImpl brt = new BirtReportServiceImpl();
+			pathName = pathDir + r.getName() + ".rptdesign";
 			
-			brt.prepareDataset(report);
+			prepareDatasets(reportData.getDataSets(), r);	
 			
-/*			ReportDesignHandle dezign = brt.openReportDesign(report.getReportDesignPath());
-			
-			OdaDataSourceHandle  dataSource = (OdaDataSourceHandle) dezign.getAllDataSources().get(0);
-			dataSource.setProperty("URI", "C:\\Projects\\OpenMRS\\BIRT\\dsnimi\\Person Count.csv");	
-			
-
-			OdaDataSetHandle  dataSet = (OdaDataSetHandle) dezign.getAllDataSets().get(0);
-			dataSet.setProperty("queryText", "select 'person_id', 'birthdate' from 'file:/C:/Projects/OpenMRS/BIRT/dsnimi/Person Count.csv' : {'person_id',\"person_id\",STRING;\"birthdate\",\"birthdate\",STRING}");
-*/
-			
-			BirtReportService reportService = (BirtReportService) Context.getService(BirtReportService.class);			
-
-			reportService.generateReport(report);
+			modifyReport(r);						
+					
+			executeReport();
 
 			// Get a reference to the report output file to be copied to the response
-			InputStream fileInputStream = new FileInputStream(report.getOutputFile());
+			InputStream fileInputStream = new FileInputStream(new File(pathDir + "my_report.pdf"));
 
 			FileCopyUtils.copy(fileInputStream, out);
 
@@ -155,53 +139,298 @@ public class BirtTemplateRenderer extends ReportTemplateRenderer {
 		}
 
 	}
+	
+	String[] qtext = null;
+	public void prepareDatasets(Map<String, DataSet> datasets, ReportDesignResource designResource) {
+		Iterator<Entry<String, DataSet>> iter = datasets.entrySet().iterator();
+		
+		while (iter.hasNext()) {
+			Map.Entry mEntry = (Map.Entry) iter.next();
+			mEntry.getKey();
+			
+			DataSet dataset = (DataSet) mEntry.getValue();
+			DataSetRow datasetRow = dataset.iterator().next();
+			
+			List<DataSetColumn> columns = dataset.getMetaData().getColumns();	
+			
+			try {
+				FileWriter w = new FileWriter(pathDir + designResource.getName() + ".csv");
+				
+				qtext = new String[3];
+				int i = 0;
+				// header row
+				w.append(getBeforeRowDelimiter());
+				for (DataSetColumn column : columns) {
+					w.append(getBeforeColumnDelimiter());
+					w.append(escape(column.getName()));
+					w.append(getAfterColumnDelimiter());
+					qtext[i] = column.getName();
+					i++;					
+				}
+				w.append(getAfterRowDelimiter());
+				
+				// data rows
+				for (DataSetRow row : dataset) {
+					w.append(getBeforeRowDelimiter());
+					for (DataSetColumn column : columns) {
+						Object colValue = row.getColumnValue(column);
+						w.append(getBeforeColumnDelimiter());
+						if (colValue != null) {
+							if (colValue instanceof Cohort) {
+								w.append(escape(Integer.toString(((Cohort) colValue).size())));
+							} else if (colValue instanceof IndicatorResult) {
+								w.append(((IndicatorResult) colValue).getValue().toString());
+							}
+							else {
+								// this check is because a logic EmptyResult .toString() -> null
+								String temp = escape(colValue.toString());
+								if (temp != null)
+									w.append(temp);
+							}
+						}
+						w.append(getAfterColumnDelimiter());
+					}
+					w.append(getAfterRowDelimiter());
+				}
+				
+				w.close();
+			} catch (IOException e) {
+				throw new RenderingException("Error: " + e, e);
+			}
+		}
+	}
+	
+	/**
+	 * @see DelimitedTextReportRenderer#getBeforeColumnDelimiter()
+	 */
+	public String getBeforeColumnDelimiter() {
+		return "\"";
+	}
+	
+	/**
+	 * @see DelimitedTextReportRenderer#getBeforeRowDelimiter()
+	 */
+	public String getBeforeRowDelimiter() {
+		return "";
+	}
+	
+	/**
+	 * @see DelimitedTextReportRenderer#getAfterRowDelimiter()
+	 */
+	public String getAfterRowDelimiter() {
+		return "\n";
+	}
+	
+	/**
+	 * Convenience method used to escape a string of text.
+	 * 
+	 * @param	text 	The text to escape.
+	 * @return	The escaped text.
+	 */
+	public String escape(String text) {
+		if (text == null) {
+			return null;
+		}
+		else {
+			return text.replaceAll("\"", "\\\"");
+		}
+	}	
+	
+	/**
+	 * @see DelimitedTextReportRenderer#getAfterColumnDelimiter()
+	 */
+	public String getAfterColumnDelimiter() {
+		return "\",";
+	}
+	
+	/**
+	 * Modify a report based on the attributes of the given report object.
+	 * 
+	 */	
+	public void modifyReport(ReportDesignResource designResource) {
+		FileOutputStream fos = null;
+		try {
+			fos = new FileOutputStream(pathName);
+			fos.write(designResource.getContents());
+			fos.close();
+			
+			ReportDesignHandle reportDesign = getReportDesign(pathName);
+			
+			if ( reportDesign != null ) { 
 
+				for (Iterator iterator = reportDesign.getAllDataSets().iterator(); iterator.hasNext(); ) { 
+					Object obj = iterator.next();
+
+
+					if ( obj instanceof OdaDataSetHandle) { 
+						OdaDataSetHandle datasetHandle = (OdaDataSetHandle) obj;					
+
+						// Flat File data set (reset the HOME property) 
+						if ("org.eclipse.datatools.connectivity.oda.flatfile.dataSet".equals(datasetHandle.getExtensionID())) { 
+							
+							// First we need to set the data source to the dataset's current directory
+							String uriPath = "file:/" + pathDir + designResource.getName() + ".csv";
+							
+							datasetHandle.getDataSource().setProperty("URI", uriPath.replace(File.separator, "/"));
+							//datasetHandle.setProperty("queryText", "select 'field_type_id', 'name' from '" + pathDir + designResource.getName() + ".csv'" + " : {'field_type_id',\"field_type_id\",STRING;\"name\",\"name\",STRING}");
+							
+							// TODO Refactor to use a better query parser ... 
+							// this one does not handle more complex queries 
+							log.debug("Data set query [BEFORE]:\n" + datasetHandle.getQueryText());
+
+							// Create the query object and change the table name
+							BirtDataSetQuery datasetQuery = new BirtDataSetQuery(datasetHandle.getQueryText());					
+							datasetQuery.setTable(designResource.getName() + ".csv");
+							
+							StringBuilder sb = new StringBuilder();
+							sb.append("SELECT ");
+							for (int i = 0; i < qtext.length - 1; i++){
+								sb.append("\"");
+								if(i == qtext.length-2)
+									sb.append(qtext[i]).append("\"");
+								else
+									sb.append(qtext[i]).append("\"").append(",");								
+							}
+							sb.append(" FROM ");
+							sb.append("\"");
+							sb.append("file:/");
+							sb.append(pathDir.replace(File.separator, "/"));
+							sb.append(designResource.getName() + ".csv");
+							sb.append("\"");
+							sb.append(" : {");
+							for (int i = 0; i < qtext.length-1; i++){
+								if(i == qtext.length-2)
+									sb.append("\"").append(qtext[i]).append("\"").append(",").append("\"").append(qtext[i]).append("\"").append(",").append("STRING");
+								else
+									sb.append("\"").append(qtext[i]).append("\"").append(",").append("\"").append(qtext[i]).append("\"").append(",").append("STRING").append(";");
+							}
+							sb.append("}");
+
+							//datasetHandle.setQueryText(datasetQuery.getQueryText());
+							datasetHandle.setQueryText(sb.toString());
+							log.debug("Data set query [AFTER]:\n" + datasetHandle.getQueryText());
+						} 
+	
+					}					
+				}
+				reportDesign.checkReport();
+				reportDesign.save();		
+				reportDesign.close();
+			} else { 
+				log.debug("Report design " + "report.getReportDesignPath()" + " does not exists");
+			}
+			
+		} catch (Exception e) {
+			
+		}
+	}
+	
+	
 	/**
 	 * Generate a report based on the attributes of the given report object.
 	 * 
 	 * @param	report	the report to generate
 	 */
-	public void generateReport(BirtReport report) {
-		log.debug("Generating output for report " + report + ", hashcode " + report.hashCode());
+	public void executeReport() {
+		//log.debug("Generating output for report " + designResource + ", hashcode " + designResource.hashCode());
 		IRunAndRenderTask task = null;
-		try {     		
-			// Prepares the dataset for use within the BIRT engine 
-			// (i.e. exports csv file, sets username/password for JDBC dataset)
-			//prepareDataset(report);
-
-			// Get the report engine that will be used to render the report
+		try {			
+		
+		// Get the report engine that will be used to render the report
 			IReportEngine engine = BirtConfiguration.getReportEngine();	    		
 
 			// Open the report design
-			IReportRunnable reportRunnable = engine.openReportDesign(report.getReportDesignPath());
-
+			IReportRunnable reportRunnable = engine.openReportDesign(pathName);
+				
 			// Create a report rendering task
 			task = engine.createRunAndRenderTask(reportRunnable);
-			task.setParameterValues(report.getParameterValues());
-			task.validateParameters();    		
-			task.setRenderOption(BirtConfiguration.getRenderOption(report));
+			task.setParameterValues(null);
+			// Validate runtime parameters
+			task.validateParameters();
+			
+			//task.setRenderOption(BirtConfiguration.getRenderOption(report));
+			PDFRenderOption options = new PDFRenderOption();
+			options.setOutputFileName(pathDir + "my_report.pdf");
+			options.setOutputFormat("pdf");
+			task.setRenderOption(options);
+			//task.setRenderOption(BirtConfiguration.getRenderOption(report));    
+			
+			// Render report design
 			task.run();
+			task.close();
+			engine.destroy();
 
-			// Add errors to the report object
+/*			// Add errors to the report object
 			if (task.getErrors() != null && !task.getErrors().isEmpty()) {
 				report.setErrors(task.getErrors());
 			}
-
+*/
 			// Set the output file 
-			report.setOutputFile(new File(report.getOutputFilename()));	    	
+			new File(pathName);
+			//report.setOutputFile(new File(PathName));	    	
 
-			log.debug("Output file: " + report.getOutputFile().getAbsolutePath());
+			//log.debug("Output file: " + report.getOutputFile().getAbsolutePath());
 		} 
 		catch (EngineException e) { 
 			log.error("Unable to generate report due to a BIRT Exception: " + e.getMessage(), e);
 			throw new BirtReportException("Unable to generate report due to a BIRT Exception: " + e.getMessage(), e);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		finally { 
 			if (task != null) task.close();
 		}
 	}
+	
+	/**
+	 * Gets a report design file by opening an existing file or creating a new design file).
+	 * 
+	 * @param reportPath
+	 * @return
+	 */
+	public ReportDesignHandle getReportDesign(String reportPath) { 
+		ReportDesignHandle handle = null;
+		try { 
 
+			handle = openReportDesign(reportPath);	    	
 
+			// Removed this functionality because it was sometimes overwriting report design files
+			// that EXIST but were not found.  Not sure why/how the report design was not found.
 
+			/*if (handle==null) { 
+	    		log.debug("Could not open report design " + reportPath);
+	    		handle = createReportDesign(reportPath);
+	    	}*/
 
+		} catch (Exception e) { 
+			// TODO for debugging, re-throw exception
+			log.warn("Unable to open report design at location " + reportPath);	 
+			/* ignore for now since all reports start with no report design */
+		}
+		return handle;
+	}
+	
+	/**
+	 * Open an existing report
+	 * 
+	 * @param reportPath	path to the report
+	 * @return	a report design handle
+	 */
+	public ReportDesignHandle openReportDesign(String reportPath) {
+		ReportDesignHandle handle = null;
+		try {
+			IDesignEngine designEngine = BirtConfiguration.getDesignEngine();	
+			handle = designEngine.newSessionHandle(ULocale.ENGLISH).openDesign(reportPath);
+		}
+		catch (DesignFileException e) { 
+			// Ignore for now
+			log.debug("Could not open report design " + reportPath + ": " + e.getMessage());
+			//throw new BirtReportException(e);
+		}
+		return handle;
+	}
+
+	
+	
 }
