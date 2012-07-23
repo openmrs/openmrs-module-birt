108e3b55237549a52f8dbefb5c73b51e762acde7
justin <justin@5bac5841-c719-aa4e-b3fe-cce5062f897a>
2012-07-23 19:45:55 +0000

    Added scaffolding for a simple spring controller.

---
+++ web/src/org/openmrs/module/birt/web/controller/DownloadReportDesignController.java
@@ -0,0 +1,30 @@
+package org.openmrs.module.birt.web.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+
+/**
+ *
+ */
+public class DownloadReportDesignController implements Controller {
+	
+    /** Logger for this class and subclasses */
+    protected final Log log = LogFactory.getLog(getClass());
+
+	public ModelAndView handleRequest(HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+    
+}
+
+
